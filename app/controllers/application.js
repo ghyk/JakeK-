1694f50d58f3f8780db6f2c14ee717f98b572db7
ghyk <popo.jake1@gmail.com>
2016-06-14 13:37:54 -0700

    nearly done

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,18 +1,18 @@
 import Ember from 'ember';
 
 var Photo = Ember.Object.extend({
-		title: '',
-		username: '',
-		owner: '',
-		id: '',
-		farm: 0,
-		secret:'',
-		server:'',
-		url: function(){
-			return "https://farm"+this.get('farm')+
-			".staticflickr.com/"+this.get('server')+
-			"/"+this.get('id')+"_"+this.get('secret')+"_b.jpg";
-		}.property('farm','server','id','secret'),
+	title: '',
+	username: '',
+	owner: '',
+	id: '',
+	farm: 0,
+	secret: '',
+	server: '',
+	url: function(){
+		return "https://farm"+this.get('farm')+
+		".staticflickr.com/"+this.get('server')+
+		"/"+this.get('id')+"_"+this.get('secret')+"_b.jpg";
+	}.property('farm','server','id','secret'),
 });
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
@@ -70,27 +70,28 @@ export default Ember.Controller.extend ({
 				this.get('filteredPhotos');
 		},
 		getPhotos: function(){
-			var apiKey = '828f7dd0ab21e6018c8da457ff4b1e4e';
-			var host = 'https://api,flickr.com/services/rest/';
-			var method = "flickr.tags.getClusterPhotos";
-			var tag = "yup";
-			var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tag="+tag+"&format=json&nojsoncallback=1";
-			var photos = this.get ('photos');
-			Ember.$.getJSON(requestURL, function(data){
-
-				console.log(data);
-				data.photos.photo.map(function(photo) {
-					var newPhotoItem = Photo.create({
-						title: photo.title,
-						username: photo.username,
-						owner: photo.owner,
-						id: photo.id,
-						farm: photo.farm,
-						secret: photo.secret,
-						server: photo.server,
-
-					});
-					photos.pushObject(newPhotoItem);
+	var apiKey = '828f7dd0ab21e6018c8da457ff4b1e4e';
+	var host = 'https://api.flickr.com/services/rest/';
+	var method = "flickr.tags.getClusterPhotos";
+	var tag = "hi";
+	var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tag="+tag+"&format=json&nojsoncallback=1";
+	var photos = this.get('photos');
+	Ember.$.getJSON(requestURL, function(data){
+		//callback for successfully completed requests
+		console.log(data);
+		data.photos.photo.map(function(photo) {
+			var newPhotoItem = Photo.create({
+				title: photo.title,
+				username: photo.username,
+				//flickr extra data
+				owner: photo.owner,
+				//flickr url data
+				id: photo.id,
+				farm: photo.farm,
+				secret: photo.secret,
+				server: photo.server,
+			});
+			photos.pushObject(newPhotoItem);
 				})
 			});
 		}
